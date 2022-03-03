5b8bf5483774a17daced0e47c8db6dd5589265d8
britt-thom <brittany.thomas@smsja.net>
2022-03-03 13:02:11 -0500

    Variables and Data Types

--- src/Tutorial/Main.java
+++ src/Tutorial/Main.java
@@ -1,4 +1,16 @@
 package Tutorial;
 
-public class main {
+public class Main {
+    public static void main(String[] args) {
+        //primitive
+       int hello_world = 5;
+       double num2 = 5.0;
+       boolean b = false;
+       char c = 'h';
+       String str = "9";
+
+       int tim = hello_world;
+       System.out.println(str);
+
+    }
 }
