8cf59c90ea3be2eabdfd6d2b6320f5c8f145f448
britt-thom <brittany.thomas@smsja.net>
2022-03-04 11:32:14 -0500

    Maps and Hashmaps

--- src/Tutorial/Main.java
+++ src/Tutorial/Main.java
@@ -3,17 +3,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    ArrayList<Integer> t = new ArrayList<>();
-    t.add(1);
-    t.add(2);
-    t.add(1);
-    t.add(2);
-    t.add(1);
-    t.add(2);
-    t.get(0);
-    t.set(1, 5);
-    t.subList(1,3);
+    int[] x = {-99, 5, 6, 3, 2, 1, 7, 8, 0};
 
-    System.out.println(t.subList(1,3));
+    Arrays.sort(x,3,6);
+
+    for (int i:x) {
+        System.out.print(i + ",");
+    }
     }
 }
