f889ac30aa84fe96f57c07df1b89749fd23bdb7d
britt-thom <brittany.thomas@smsja.net>
2022-03-04 17:55:30 -0500

    Overloading and comparing objects

--- src/Tutorial/Main.java
+++ src/Tutorial/Main.java
@@ -1,10 +1,12 @@
 package Tutorial;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Cat tim = new Cat("tim", 18, 100);
-        tim.speak();
+        Student joe = new Student("Joe");
+        Student bill = new Student("Bill");
+        Student tim = new Student("Tim");
+
+        System.out.println(tim);
     }
 }
