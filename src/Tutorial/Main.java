3939b2a776ea89f67b9c18fed29624069a176aa5
britt-thom <brittany.thomas@smsja.net>
2022-03-03 21:57:32 -0500

    Arrays

--- src/Tutorial/Main.java
+++ src/Tutorial/Main.java
@@ -3,28 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[] newArr = new String[5];
+        newArr[0] = "hello";
+        newArr[1] = "hi";
+        newArr[2] = "tim";
+        newArr[3] = "bill";
+        newArr[4] = "joe";
 
-      System.out.println("Input your age: ");
-      Scanner sc = new Scanner(System.in);
-      String s = sc.nextLine();
-      int age = Integer.parseInt(s);
-
-      if (age >= 18) {
-          System.out.println("Input your fav food: ");
-          String food = sc.nextLine();
-          if (food.equals("pizza")) {
-              System.out.println("Mine too");
-          }
-          else {
-              System.out.println("Not mine");
-          }
-      }
-      else if (age >= 13) {
-          System.out.println("You are a teenager");
-      }
-      else {
-          System.out.println("You are not a teenager or an adult");
-      }
-
+        String x = newArr[4];
+        System.out.println(x);
     }
 }
