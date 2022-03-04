d77d0dbe7a36abe96e39410af37bc41175799fde
britt-thom <brittany.thomas@smsja.net>
2022-03-04 12:03:16 -0500

    Intro to Objects

--- src/Tutorial/Main.java
+++ src/Tutorial/Main.java
@@ -1,14 +1,30 @@
 package Tutorial;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    int[] x = {-99, 5, 6, 3, 2, 1, 7, 8, 0};
+        Scanner sc = new Scanner(System.in);
+        //sc.next();
+        String h = "hello";
+        h.length();
 
-    Arrays.sort(x,3,6);
-
-    for (int i:x) {
-        System.out.print(i + ",");
+        System.out.println(str("hi"));
+        System.out.println(add2(6));
+        tim("Timmmmmm", 4);
     }
+
+    public static void tim(String str, int x) {
+        for (int i = 0; i < x; i++) {
+            System.out.println(str);
+        }
+    }
+
+    public static int add2(int x) {
+        return x + 2;
+    }
+
+    public static String str(String x) {
+        return x + "!";
     }
 }
